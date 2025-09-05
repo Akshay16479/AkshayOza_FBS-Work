27670cec4a9f0d8bb6768d545d364e65d8d2c16c
Akshay16479 <ozaakshay880@gmail.com>
2025-09-05 14:38:35 +0530

    Add files via upload

---
+++ C Assignment/FBS Assignment6/FBS Assignment 6A/FunctionType1- Assignment0_Q3.c
@@ -0,0 +1,15 @@
+void convert();
+void main()
+{
+ convert();
+}
+
+void convert()
+{
+	int c,f;
+     c=15;
+
+   f=(c*9/5)+32;
+
+   printf("%d",f);
+}
---
+++ C Assignment/FBS Assignment6/FBS Assignment 6A/FunctionType1- Assignment0_Q7.c
@@ -0,0 +1,25 @@
+void arearec(),perirec();
+
+void main()
+{
+	
+arearec();
+
+perirec();
+
+}
+
+void arearec()
+{
+int length=13, breadth=17, area;
+area=length*breadth;
+printf("%d\n", area);
+}
+
+void perirec()
+{
+int length=13, breadth=17, perimeter;
+perimeter=2*(length*breadth);
+printf("%d",perimeter);
+}
+
