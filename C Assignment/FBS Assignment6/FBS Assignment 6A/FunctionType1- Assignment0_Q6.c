834447ef862147905ec50ab6c8e4162a0d706ce6
Akshay16479 <ozaakshay880@gmail.com>
2025-09-05 14:42:56 +0530

    Add files via upload

---
+++ C Assignment/FBS Assignment6/FBS Assignment 6A/FunctionType1- Assignment0_Q6.c
@@ -0,0 +1,20 @@
+void sq(),cube();
+void main()
+{
+sq();
+cube();
+}
+
+void sq()
+{
+int a=4,square;
+square=(a*a);
+printf("%d\n", square);
+}
+
+void cube()
+{
+int a=4,cube;
+cube=(a*a*a);
+printf("%d", cube);
+}
