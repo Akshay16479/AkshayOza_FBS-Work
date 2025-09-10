bb262ddd879c5d8ec62bc7a8f1cf549dae5cfb63
Akshay16479 <ozaakshay880@gmail.com>
2025-09-10 23:50:55 +0530

    Add files via upload

---
+++ C Assignment/_FBS Assignment 7/Question 3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function declaration
+void sumRange(int* , int );
+
+void main()
+{
+    int num = 1;
+
+    // Function call
+    sumRange(&num, 5);
+}
+
+// Function definition
+void sumRange(int *num, int end)
+{
+    int sum = 0;
+
+    while (*num <= end)
+    {
+        sum = sum + *num;
+        (*num)++;
+    }
+
+    printf("%d", sum);
+}
+
