e6539f6c2f4d11163125c91103f0bd776b13bc6d
Akshay16479 <ozaakshay880@gmail.com>
2025-11-08 19:34:57 +0530

    Shape.java

--- Java/Function Overloading Assignment/shape.java
+++ Java/Function Overloading Assignment/shape.java
@@ -1,20 +1,17 @@
 class Shape {
 
-    // Area of Triangle
-
-    void area(double base, double height) {
+    // Triangle
+      void area(double base, double height) {
         System.out.println("Area of Triangle: " + (0.5 * base * height));
     }
 
-    // Area of Rectangle
-
+    // Rectangle
     void area(int length, int breadth) {
         System.out.println("Area of Rectangle: " + (length * breadth));
     }
 
-    // Area of Circle
-
-    void area(double radius) {
+    //Circle
+      void area(double radius) {
         System.out.println("Area of Circle: " + (3.14 * radius * radius));
     }
 
@@ -32,3 +29,4 @@ class test {
         s.area(7.0);   
     }
 }
+
