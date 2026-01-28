5fe316f1a77c86f84c84316a6679f41ad803b873
Akshay Rajesh Oza <ozaakshay880@gmail.com>
2026-01-28 19:57:56 +0530

    Delete Advanced Java/JDBC Work/Login Validation directory

--- Advanced Java/JDBC Work/Login Validation/src/com/dao/DAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.beans.User;
-
-public interface DAOInterface 
-{
-    public void insertUser(User u);
-    public boolean retriveData(String username);
-}
--- Advanced Java/JDBC Work/Login Validation/src/com/services/LoginValidationServices.java
+++
@@ -1,8 +0,0 @@
-package com.services;
-
-public interface LoginValidationServices {
-	
-	public void createUser(String username,String password);
-	public boolean validateUser(String username,String password);
-
-}
