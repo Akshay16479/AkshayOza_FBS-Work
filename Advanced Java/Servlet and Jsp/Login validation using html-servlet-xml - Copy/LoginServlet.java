07476627f7aabb90ff81cdb0668e2a7c004c0b4b
Akshay Rajesh Oza <ozaakshay880@gmail.com>
2026-02-06 21:04:25 +0530

    Move DB credentials to web.xml context params

---
+++ Advanced Java/Servlet and Jsp/Login validation using html-servlet-xml - Copy/LoginServlet.java
@@ -0,0 +1,82 @@
+package com.loginvalidation;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginServlet extends HttpServlet {
+
+	Connection con;
+	PreparedStatement pst;
+	ResultSet rs;
+
+	public void init() {
+		try {
+			
+			//getting db config info from xml middleware file
+			String driverclass=getServletConfig().getServletContext().getInitParameter("driver");
+			String url=getServletConfig().getServletContext().getInitParameter("url");
+			String user=getServletConfig().getServletContext().getInitParameter("username");
+			String pass=getServletConfig().getServletContext().getInitParameter("password");
+
+			Class.forName(driverclass);
+			con = DriverManager.getConnection(url, user, pass);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// user data from html
+		try {
+			String u = request.getParameter("username");
+			String p = request.getParameter("password");
+
+			// get data from db as connection is done in init()
+			pst = con.prepareStatement("SELECT * from user where username= ? ");
+			pst.setString(1, u);
+			rs = pst.executeQuery();
+
+			// data print on browser
+			PrintWriter out = response.getWriter();
+
+			if (rs.next()) {
+				out.println("<h1>Valid User</h1>");
+			} else {
+				out.println("<h1>Invalid User</h1>");
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	
+	
+	public void destroy() {
+		try {
+			con.close(); // releasing connection not destroying
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
